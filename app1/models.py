91b01a3c304c9c126dae4d13d79d7d2dd26a711f
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2019-07-17 00:23:53 +0200

    Finish login/logout

--- app1/models.py
+++ app1/models.py
@@ -1,7 +1,11 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from . import db
+from . import login
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     my_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,6 +14,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_has = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
@@ -20,3 +30,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(this_id):
+    return User.query.get(int(this_id))
