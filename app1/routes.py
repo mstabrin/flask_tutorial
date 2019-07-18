346536c165f7e484d31f0f2b563d99717fa96796
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2019-07-19 01:40:21 +0200

    Fixed index.html

--- app1/routes.py
+++ app1/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, login_required, logout_user
 from . import app, db
-from .forms import LoginForm, RegistrationForm, EditProfileForm
-from .models import User
+from .forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from .models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -15,21 +15,20 @@ def before_request():
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Markus'}
-    posts = [
-        {
-            'author': {'username', 'John'},
-            'body': 'Beautiful day in Portland!'
-            },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-            }
-        ]
-    return render_template('index.html', title='Home Page', posts=posts)
+    form = PostForm()
+    print('yeah')
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', posts=posts, form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -130,3 +129,10 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
